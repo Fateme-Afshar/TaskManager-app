75b6fcab0fbea5f18ae9c2694dfe62e0ebb8db9c
FatemeAfshar20 <uttab4760@gmail.com>
2021-04-09 21:41:58 -0700

    Adding dagger dependency and ApplicationGraph class.

---
+++ app/src/main/java/com/example/taskmaneger/TaskManagerApplication.java
@@ -0,0 +1,11 @@
+package com.example.taskmaneger;
+
+import android.app.Application;
+
+public class TaskManagerApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
---
+++ app/src/main/java/com/example/taskmaneger/di/ApplicationGraph.java
@@ -0,0 +1,8 @@
+package com.example.taskmaneger.di;
+
+import dagger.Component;
+
+@Component
+public interface ApplicationGraph {
+
+}
